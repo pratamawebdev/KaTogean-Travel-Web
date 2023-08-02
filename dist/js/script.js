b1194e7b107463c6639fc559321450c3b8b37153
willy0910 <willypratama231@gmail.com>
2023-08-02 11:38:01 +0800

    First commit

---
+++ dist/js/script.js
@@ -0,0 +1,44 @@
+/* Navbar - Scroll Start */
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+});
+
+/* Navbar - Scroll End */
+
+let intro = document.querySelector(".intro");
+let logo = document.querySelector(".logo");
+let logoSpan = document.querySelectorAll(".logo-parts");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, index) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (index + 1) * 100);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, index) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (span + 1) * 50);
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      intro.style.top = "-100vh";
+    }, 2300);
+  });
+});
+
+var myAlert = document.getElementById("myAlert");
+myAlert.style.display = "none";
+
+function myFunction() {
+  myAlert.style.display = "block";
+}
